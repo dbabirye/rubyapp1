59a775ef270524f6f81442ba0ce0c5953cd6d65e
root <root@linux-vazv.site>
2012-11-19 14:05:37 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App1::Application.config.secret_token = 'b453168b08b89be44377a7d1d6f164896ca15bab195986f5e997fc3daa0dddff81810586ce97738c31cd6a9f5edb2f94a0f11b3fc8895205ed90cd35d66c707d'
